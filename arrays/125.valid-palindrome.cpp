842b6d168d34002f3ddc00c1aed60271225ada92
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-03-16 14:32:46 +0530

    solved Leetcode 125: Valid Palindrome

---
+++ arrays/125.valid-palindrome.cpp
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=125 lang=cpp
+ *
+ * [125] Valid Palindrome
+ */
+
+// @lc code=start
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int start = 0;
+        int end = s.length() - 1;
+        while (start <= end ) {
+            if (isalnum(s[start]) == 0 ) {
+                start++;
+                continue;
+            }
+            if (isalnum(s[end]) == 0 ) {
+                end--;
+                continue;
+            }
+            if(tolower(s[start]) != tolower(s[end])) {
+                return false;
+            }else {
+                start++;
+                end--;
+            }
+        }
+        return true;
+    }
+};
+// @lc code=end
+
