fd1505a1157be4c784fe3cbbf31747ac3bc23e04
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-03-18 19:32:50 +0530

    solved the sliding window problem

--- arrays/239.sliding-window-maximum.cpp
+++ arrays/239.sliding-window-maximum.cpp
@@ -6,12 +6,37 @@
 
 // @lc code=start
 #include <vector>
+#include <deque>
 using namespace std;
 
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        
+        int n = nums.size();
+
+        deque<int> deq;
+
+        vector<int> result;
+
+        for (int i = 0; i<n; i++){
+            // step1: make space for nums[i]
+            while(!deq.empty() && deq.front() <= i-k) {
+                deq.pop_front();
+            }
+            //step2: pop smaller elements
+            while(!deq.empty() && nums[i] > nums[deq.back()]) {
+                deq.pop_back();
+            }
+            //step3: add nums[i]
+            deq.push_back(i);
+            
+            //step4: add max to result
+            if (i >= k-1) {
+                result.push_back(nums[deq.front()]);
+            }
+        }
+        return result;
+
     }
 };
 // @lc code=end
