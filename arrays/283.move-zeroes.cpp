24bec473b1b1f9722e3ff422d3aa135d97aab6b5
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-03-19 21:58:13 +0530

    solving the Move Zeroes Question

---
+++ arrays/283.move-zeroes.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=283 lang=cpp
+ *
+ * [283] Move Zeroes
+ */
+
+// @lc code=start
+#include <vector> 
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+
+        
+
+    }
+};
+// @lc code=end
+
