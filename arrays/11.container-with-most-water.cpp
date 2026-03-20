b45c50e5362795aa5ca898707bc90e1ba6f3e4f2
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-03-20 21:41:08 +0530

    Solved Leetcode:11. Container With Most Water

--- arrays/11.container-with-most-water.cpp
+++ arrays/11.container-with-most-water.cpp
@@ -11,7 +11,29 @@ using namespace std;
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        
+        int n = height.size();
+
+        int i = 0, j = n-1;
+
+        int maxWater = 0;
+
+        while(i<j) {
+            int w = j-i;
+
+            int h = min(height[i], height[j]);
+
+            int area = w*h;
+
+            maxWater = max(maxWater, area);
+
+            if(height[i] > height[j]) {
+                j--;
+            } else {
+                i++;
+            }
+
+        }
+        return maxWater;
     }
 };
 // @lc code=end
