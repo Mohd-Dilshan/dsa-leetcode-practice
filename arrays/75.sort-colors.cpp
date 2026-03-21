c7d969ba91636f586573c472c2acd3c00d240104
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-03-21 21:18:37 +0530

    Solved leetcode 71:sorted colors

--- arrays/75.sort-colors.cpp
+++ arrays/75.sort-colors.cpp
@@ -10,7 +10,27 @@ using namespace std;
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        
+        int n = nums.size();
+
+        int i = 0;  // index of the last 0
+        int j = 0; // index of the last 1 
+        int k = n-1; // index of the last 2
+
+        while (j <= k )  {
+            if (nums[j] == 1 ) {
+                j++;
+            } else if (nums[j] == 2) {
+                swap(nums[j], nums[k]);
+                k--;
+            } else {
+                //nums[j] == 0
+                swap(nums[i], nums[j]);
+                i++;
+                j++;
+            }
+        }
+
+
     }
 };
 // @lc code=end
