a97d36472bdbf0b4cb16012150320cd6cf1c6ecc
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-03-23 21:01:49 +0530

    started with leetcode 27:remove element

---
+++ arrays/27.remove-element.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=27 lang=cpp
+ *
+ * [27] Remove Element
+ */
+
+// @lc code=start
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+         int n = nums.size();
+
+         
+    }
+};
+// @lc code=end
+
