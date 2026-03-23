fd23f06a359f4eb735448e012a627e8667ee1d5a
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-03-23 21:43:48 +0530

    Solved leetcode 27:remove element

--- arrays/27.remove-element.cpp
+++ arrays/27.remove-element.cpp
@@ -6,13 +6,48 @@
 
 // @lc code=start
 #include<vector>
+#include<algorithm>
 using namespace std;
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-         int n = nums.size();
+        //sort this array first
+        std::sort(nums.begin(), nums.end());
+        
+        //first we find the index where copies of the val start
+        int i = 0;
+        while (i<nums.size() && nums[i] != val) {
+            i++;
+        }
 
-         
+        //retrun i, if i equals nums.size();
+
+        if (i == nums.size()) {
+            return i;
+        }
+
+        // find the indices containig the copies of val
+
+        int j = i ;
+        while (j< nums.size() && nums[j] == val) {
+            j++;
+        }
+
+        // i to j-1
+
+        if (j == nums.size()) {
+            //first i values are not equal to val, 
+            // copies of val from ith index till the end of the array 
+            return i;
+        }
+
+        // copy non-val values frpm jth index to ith index
+        while (j<nums.size()) {
+            nums[i] = nums[j];
+            i++;
+            j++;
+        }
+        return i;
     }
 };
 // @lc code=end
