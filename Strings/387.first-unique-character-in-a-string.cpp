4b879a6cfa3f9d5abafce311330fbf981d131ace
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-03-25 16:04:12 +0530

    started with 387 leetcode:first unique chars in a str

---
+++ Strings/387.first-unique-character-in-a-string.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=387 lang=cpp
+ *
+ * [387] First Unique Character in a String
+ */
+
+// @lc code=start
+#include <string>
+using namespace std;
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        
+    }
+};
+// @lc code=end
+
