8b5cc9e9087ce84bbefcafe690d430e762e81510
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-03-26 16:33:04 +0530

    solved leetcode:242 valid anagram Problem.

--- Strings/242.valid-anagram.cpp
+++ Strings/242.valid-anagram.cpp
@@ -6,12 +6,39 @@
 
 // @lc code=start
 #include<string>
+#include<algorithm>
+#include<vector>
 using namespace std;
 
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        // method 1: sorting
+
+        sort (begin(s), end(s));
+        sort (begin(t), end(t));
+
+        return s==t;
         
+        //method 2: counting
+        // vector<int> count (26,0);
+
+        // for (char &ch : s) {
+        //     count[ch-'a']++;
+        // }
+
+        // for (char &ch : t) {
+        //     count[ch-'a']--;
+        // }
+        // // check if all counts are zero
+        // //fopr loop
+        // bool allZeros = all_of (begin(count), end(count), [](int element){
+    
+        //     return element==0; 
+        // });
+        // return allZeros;
+
+
     }
 };
 // @lc code=end
