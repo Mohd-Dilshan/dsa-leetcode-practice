9674e75843cc80a9af76d227af7b74aee76b8e18
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-03-26 16:13:03 +0530

    started with valid anagram

---
+++ Strings/242.valid-anagram.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=242 lang=cpp
+ *
+ * [242] Valid Anagram
+ */
+
+// @lc code=start
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+    }
+};
+// @lc code=end
+
