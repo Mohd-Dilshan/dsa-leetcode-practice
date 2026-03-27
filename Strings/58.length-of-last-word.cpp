06c1811388a4dde59b925212ed6c831c5ef53717
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-03-27 21:13:58 +0530

    Started the Length of Last Word Problem in Leetcode.

---
+++ Strings/58.length-of-last-word.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=58 lang=cpp
+ *
+ * [58] Length of Last Word
+ */
+
+// @lc code=start
+
+#include <string>
+using namespace std;
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        
+    }
+};
+// @lc code=end
+
