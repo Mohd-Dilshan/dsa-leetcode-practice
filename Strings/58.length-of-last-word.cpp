0a2330c1dc514082898f24e019013aeffb790697
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-03-27 21:38:11 +0530

    solve the leetcode:58 Length of Last Word

--- Strings/58.length-of-last-word.cpp
+++ Strings/58.length-of-last-word.cpp
@@ -11,8 +11,19 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        
+        int i = s.size() - 1;
+
+        int length = 0;
+
+        while (s[i] == ' ')  {
+            i--;
+        }
+
+        while  (i >= 0 && s[i]!= ' ') {
+            length++;
+            i--;
+        }
+        return length;
     }
 };
 // @lc code=end
-
