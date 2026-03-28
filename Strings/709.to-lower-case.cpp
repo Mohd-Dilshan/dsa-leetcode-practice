ebc7bfb5ab0eec928c393c39cc675694e2899fb6
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-03-28 18:26:39 +0530

    Soved Leetcode:709 To Lower Case Problem

--- Strings/709.to-lower-case.cpp
+++ Strings/709.to-lower-case.cpp
@@ -11,7 +11,14 @@ using namespace std;
 class Solution {
 public:
     string toLowerCase(string s) {
-        
+        int n = s.size();
+
+        for (int i = 0; i <n; i++) {
+            if (s[i] >= 'A' && s[i] <= 'Z') {
+                s[i] = s[i] -'A' + 'a';
+            }
+        }
+        return s;
     }
 };
 // @lc code=end
