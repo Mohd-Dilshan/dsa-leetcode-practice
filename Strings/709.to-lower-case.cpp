8c268b0b5e9d5156328cc04a6ca2f6c13d30c1fd
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-03-28 18:14:17 +0530

    Started with Leetcode:709 To Lower Case Problem

---
+++ Strings/709.to-lower-case.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=709 lang=cpp
+ *
+ * [709] To Lower Case
+ */
+
+// @lc code=start
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string toLowerCase(string s) {
+        
+    }
+};
+// @lc code=end
+
