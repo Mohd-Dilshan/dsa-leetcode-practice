af814b97c219bef19bedac798a2d6bc5794bc4bc
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-03-30 21:02:04 +0530

    Solved Leetcode:345.Reverse Vowels of a String

--- Strings/345.reverse-vowels-of-a-string.cpp
+++ Strings/345.reverse-vowels-of-a-string.cpp
@@ -6,11 +6,38 @@
 
 // @lc code=start
 #include <string>
+#include <iostream>
+#include <algorithm> 
+
 using namespace std;
 class Solution {
 public:
+
+    bool isVowel(char c) {
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+            c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+            return true;
+        }
+        return false;
+    }
     string reverseVowels(string s) {
-        
+        int n = s.length();
+
+        int i = 0, j = n - 1;
+
+        while (i < j) {
+            if (!isVowel(s[i])) { 
+                i++;
+            } else if (!isVowel(s[j])) { 
+                j--;
+            } else {
+                swap(s[i], s[j]);
+                i++;
+                j--;
+
+            }
+        }
+        return s;
     }
 };
 // @lc code=end
