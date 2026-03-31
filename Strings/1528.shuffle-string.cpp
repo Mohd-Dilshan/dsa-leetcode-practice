1a1f116a47809879950166d7af2e577a31802b59
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-03-31 21:41:46 +0530

    Solved Leetcode:1528 Shuffle  string

--- Strings/1528.shuffle-string.cpp
+++ Strings/1528.shuffle-string.cpp
@@ -12,7 +12,19 @@ using namespace std;
 class Solution {
 public:
     string restoreString(string s, vector<int>& indices) {
-        
+        // 1. Create a new character array to hold the shuffled characters
+        string str(s.length(), ' ');
+
+        // 2. Iterate through the indices and place each character in its correct position
+
+        for (int i = 0; i < s.length(); i++) {
+            str[indices[i]] = s[i];
+        }
+
+        // 3. Return the shuffled string
+        return str;
+
+
     }
 };
 // @lc code=end
