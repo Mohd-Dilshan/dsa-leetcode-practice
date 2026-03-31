eddc927f407f7fd1d910e614d764fc66b15a6374
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-03-31 21:25:31 +0530

    Started with Leetcode:1528 Shuffle  string

---
+++ Strings/1528.shuffle-string.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=1528 lang=cpp
+ *
+ * [1528] Shuffle String
+ */
+
+// @lc code=start
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        
+    }
+};
+// @lc code=end
+
