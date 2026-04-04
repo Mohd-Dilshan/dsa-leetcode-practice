137de39563c5b5c987039e398a851af2951d4fc0
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-04-04 14:29:20 +0530

    Started with Leetcode :724 Find Pivot Index

---
+++ arrays/724.find-pivot-index.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=724 lang=cpp
+ *
+ * [724] Find Pivot Index
+ */
+
+// @lc code=start
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        
+    }
+};
+// @lc code=end
+
