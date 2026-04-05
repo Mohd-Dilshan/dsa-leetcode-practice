47f8273a57ad9d9708ac2e9259ddec3770355984
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-04-05 22:04:13 +0530

    Started with Leetcode :66 Plus One

---
+++ arrays/66.plus-one.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=66 lang=cpp
+ *
+ * [66] Plus One
+ */
+
+// @lc code=start
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        
+    }
+};
+// @lc code=end
+
