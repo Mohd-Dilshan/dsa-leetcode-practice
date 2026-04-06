18c17fe5fd11fc45e413f563cfc6f93a8e2856bb
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-04-06 15:08:45 +0530

    solved Leetcode:217 Contains Duplicate

--- array_str_mixed/217.contains-duplicate.cpp
+++ array_str_mixed/217.contains-duplicate.cpp
@@ -6,11 +6,26 @@
 
 // @lc code=start
 #include <vector>
+#include <unordered_set>
+
 using namespace std;
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        
+        //Create HashSet to integers
+
+        unordered_set<int> intSet;
+
+        //iterate over each element in the array
+        for (int num : nums) {
+            //check if the number is already in the set
+            if (intSet.find(num) != intSet.end()) {
+                return true; //duplicate found
+            }
+            //add the number to the set
+            intSet.insert(num);
+        }
+        return false; //no duplicates found   
     }
 };
 // @lc code=end
