67a69c8a6d604be900255fad1a44eef5cbf7be5b
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-04-07 14:57:54 +0530

    Solved Leetcode : 268 Missing Number

--- array_str_mixed/268.missing-number.cpp
+++ array_str_mixed/268.missing-number.cpp
@@ -6,11 +6,53 @@
 
 // @lc code=start
 #include <vector>
+#include <algorithm>
 using namespace std;
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         
+        int n = nums.size();
+        //Method 1: Using sum of n natural numbers
+
+        // int sum = n*(n+1)/2;
+
+        // for (int i = 0; i<n; i++) {
+        //     sum -= nums[i];
+        // }
+        // return sum;
+
+
+        //Method 2: Using XOR
+        // int result = n;
+
+        // for (int i = 0; i<n ; i++){
+        //     result ^= i;
+        //     result ^= nums[i];
+        // }
+        // return result;
+
+
+        //Method 3: Using binary sort
+
+        sort (nums.begin(), nums.end());
+        int result = n;
+
+        int l = 0, r= n-1;
+
+        while (l<=r) {
+            int mid = l + (r-l)/2;
+
+            if (nums[mid]>mid) {
+                result = mid;
+                r = mid-1;
+            } else {
+                l = mid+1;
+            }
+        }
+        return result;
+
+
     }
 };
 // @lc code=end
