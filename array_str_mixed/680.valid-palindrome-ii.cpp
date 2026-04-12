283f7bf3ae03fffa9cf37933933c763a94c4dfc2
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-04-13 00:08:30 +0530

    Solved Leetcode: 680 Valid Palindrome II

--- array_str_mixed/680.valid-palindrome-ii.cpp
+++ array_str_mixed/680.valid-palindrome-ii.cpp
@@ -7,10 +7,29 @@
 // @lc code=start
 #include <string>
 using namespace std;
+
 class Solution {
 public:
+    bool isPalindrome(const string& s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right]) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
     bool validPalindrome(string s) {
-        
+        int left = 0, right = s.size() - 1;
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return isPalindrome(s, left + 1, right) ||
+                       isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 };
 // @lc code=end
