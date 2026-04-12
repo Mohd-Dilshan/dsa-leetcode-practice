bb125a866a505b7da4b607e0e0f1cf86f950355d
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-04-12 23:15:51 +0530

    Started with Leetcode: 704 Binary Search

---
+++ binary_S/704.binary-search.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=704 lang=cpp
+ *
+ * [704] Binary Search
+ */
+
+// @lc code=start
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+    }
+};
+// @lc code=end
+
