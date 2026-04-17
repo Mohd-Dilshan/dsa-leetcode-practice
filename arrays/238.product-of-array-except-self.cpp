8755f70e7cf5b22cfba2aa795a89f5dab3254150
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-04-17 23:12:34 +0530

    Solved Leetcode:238 Product of Array Except Self

--- arrays/238.product-of-array-except-self.cpp
+++ arrays/238.product-of-array-except-self.cpp
@@ -10,7 +10,32 @@ using namespace std;
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        
+        //Brute Force O(n^2) time complexity
+        // vector<int> ans(nums.size(), 1);
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j<n; j++) {
+        //         if (i !=  j) {
+        //             ans[i] *= nums[j];
+        //         }
+        //     }
+        // }
+        // return ans;
+
+
+        //Optimal Solution O(n) time complexity and O(1) space complexity
+        int n = nums.size();
+        vector<int> ans(n, 1);
+        //prefix => ans
+        for (int i =1; i<n; i++){
+            ans[i] = ans[i-1] * nums[i-1];
+        }
+        //suffix
+        int suffix = 1;
+        for (int i = n-2; i>=0; i--) {
+            suffix *= nums[i+1];   // ith suffix 
+            ans[i] *= suffix;
+        }
+        return ans;
     }
 };
 // @lc code=end
