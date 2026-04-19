6fbd40080dc35f9db296edc2c1f9cc4e0fd38eb5
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-04-19 23:28:06 +0530

    Solved Leetcode : 20 Valid Parentheses

--- stack_MonoStack/20.valid-parentheses.cpp
+++ stack_MonoStack/20.valid-parentheses.cpp
@@ -12,8 +12,49 @@ using namespace std;
 class Solution {
 public:
     bool isValid(string s) {
-        
-        
+        stack<char> st;
+        // Approch 1: Use stack to store the left parentheses, and pop when meet the right parentheses.
+        // for (char &ch :s){
+        //     if (st.empty() || ch == '(' || ch == '{' || ch == '[') {
+        //         st.push(ch);
+        //         continue;
+        //     }
+        //     if (ch == ')' ) {
+        //         if (st.top() == '(')
+        //             st.pop();
+        //         else
+        //             return false;
+        //     } else if (ch == '}') {
+        //         if (st.top() == '{')
+        //             st.pop();
+        //         else
+        //             return false;
+        //     } else if (ch == ']') {
+        //         if (st.top() == '[')
+        //             st.pop();
+        //         else
+        //             return false;
+        //     }
+        // }
+        // return st.empty();
+
+
+        // Approch 2: Use stack to store the right parentheses, and pop when meet the left parentheses.
+
+        for (char &ch :s ) {
+            if (ch == '(')
+                st.push(')');
+            else if (ch == '{')
+                st.push('}');
+            else if (ch == '[')
+                st.push(']');
+            else if (st.empty() || st.top() != ch){
+                return false;
+            } else {
+                st.pop();
+            }
+        }
+        return st.empty();
     }
 };
 // @lc code=end
