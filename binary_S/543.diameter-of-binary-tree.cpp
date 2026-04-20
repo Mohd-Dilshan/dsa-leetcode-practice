8ec4e40f23b6de01ac3351a9abb012c2ea9f6093
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-04-20 22:44:25 +0530

    solved Leetcode :543 Diameter of Binary Tree

--- binary_S/543.diameter-of-binary-tree.cpp
+++ binary_S/543.diameter-of-binary-tree.cpp
@@ -4,24 +4,42 @@
  * [543] Diameter of Binary Tree
  */
 
-// @lc code=start
-/**
- * Definition for a binary tree node.
- */
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
 
+
+// @lc code=start
+#include <algorithm>
 using namespace std;
+
+// Definition for a binary tree node (usually provided by the platform).
+// struct TreeNode {
+//     int val;
+//     TreeNode *left;
+//     TreeNode *right;
+//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+// };
+
 class Solution {
 public:
+
+    int ans = 0;
+
+    int height(TreeNode* root) { // O(n)
+        if (root == nullptr) {
+            return 0;
+        }
+        int leftHeight = height(root->left);
+        int rightHeight = height(root->right);
+
+        ans = max(ans, leftHeight + rightHeight); // curret diameter of root node
+
+        return max(leftHeight, rightHeight) + 1;
+    }
+
     int diameterOfBinaryTree(TreeNode* root) {
-        
+        height(root);
+        return ans;
     }
 };
 // @lc code=end
