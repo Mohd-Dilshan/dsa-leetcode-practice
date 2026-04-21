be7ea54e60e3eb2fb47d5105dcb6c16e40c0a6ba
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-04-21 23:12:34 +0530

    Solved Leetcode : 104.Maximum Depth of Binary Tree

--- binary_S/104.maximum-depth-of-binary-tree.cpp
+++ binary_S/104.maximum-depth-of-binary-tree.cpp
@@ -19,7 +19,12 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        
+        if(root == NULL) return 0;
+
+        int leftDepth = maxDepth(root->left);
+        int rightDepth = maxDepth(root->right);
+
+        return max(leftDepth, rightDepth) + 1;
     }
 };
 // @lc code=end
