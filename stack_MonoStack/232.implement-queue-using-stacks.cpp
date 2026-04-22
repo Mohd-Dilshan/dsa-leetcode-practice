d286b06ed06082ceb62fefba4d8f648f0b14ad5c
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-04-22 23:39:23 +0530

    Solve Leetcode : 232 Implement Queue Using Stacks

--- stack_MonoStack/232.implement-queue-using-stacks.cpp
+++ stack_MonoStack/232.implement-queue-using-stacks.cpp
@@ -5,26 +5,52 @@
  */
 
 // @lc code=start
+#include <stack>
+using namespace std;
 class MyQueue {
 public:
+
+    stack<int> input;
+    stack<int> output;
+    int peekEl = -1;
+
     MyQueue() {
         
     }
     
     void push(int x) {
+        if (input.empty()) {
+            peekEl = x;
+        }
+        input.push(x); // do everything in input stack
         
     }
-    
+    //amortized O(1) time complexity
     int pop() {
-        
+        if (output.empty()) {
+            // input -> output 0(n)
+            while(!input.empty()) {
+                output.push(input.top());
+                input.pop();
+
+            }
+        }
+        int val = output.top();  // 0(1)
+        output.pop();
+        return val;
     }
     
     int peek() {
-        
+        if(output.empty())
+            return peekEl;
+        return output.top();
     }
     
     bool empty() {
-        
+        if(input.empty() && output.empty()) {
+            return true;
+        }
+        return false;
     }
 };
 
