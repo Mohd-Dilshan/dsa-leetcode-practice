305e601cd281b1f8d85220d7c39bee65c56a0924
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-04-23 22:56:53 +0530

    Solved Leetcode: 225 Implement Stack Using Queues

--- stack_MonoStack/225.implement-stack-using-queues.cpp
+++ stack_MonoStack/225.implement-stack-using-queues.cpp
@@ -5,26 +5,70 @@
  */
 
 // @lc code=start
+#include <queue>
+using namespace std;
 class MyStack {
 public:
+     // Approach 1: Two Queues
+    // queue<int> q1;
+    // queue<int> q2;
+
+    // MyStack() {
+        
+    // }
+    
+    // void push(int x) {
+    //     q2.push(x);
+    //     while(!q1.empty()) {
+    //         q2.push(q1.front());
+    //         q1.pop();
+    //     }
+    //     swap(q1, q2);
+    // }
+    
+    // int pop() {
+    //     int result = q1.front();
+    //     q1.pop();
+    //     return result;
+    // }
+    
+    // int top() {
+    //     return q1.front();
+    // }
+    
+    // bool empty() {
+    //     return q1.empty(); 
+    // }
+
+    // Approach 2: One Queue
+    queue<int> queue;
+
     MyStack() {
         
     }
     
     void push(int x) {
-        
+        queue.push(x);
+        int n  = queue.size();
+
+        for (int i = 0; i < n - 1; i++) {
+            queue.push(queue.front());
+            queue.pop();
+        }
     }
     
     int pop() {
-        
+        int x = queue.front();
+        queue.pop();
+        return x;
     }
     
     int top() {
-        
+        return queue.front();
     }
     
     bool empty() {
-        
+        return queue.empty(); 
     }
 };
 
