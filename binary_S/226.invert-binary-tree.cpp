218ee496161a3559c45d8370c15a1aa9419f5a9a
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-04-24 21:45:02 +0530

    Solved  Leetcode :226 Invert Binary Tree

--- binary_S/226.invert-binary-tree.cpp
+++ binary_S/226.invert-binary-tree.cpp
@@ -17,10 +17,24 @@
  * };
  */
 
+#include <cstddef>
+using namespace std;
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        
+        if (root == NULL) {
+            return NULL;
+        }
+        TreeNode* temp = root -> left;
+        root -> left = root -> right;
+        root -> right = temp;
+
+        invertTree(root -> left);
+        invertTree(root -> right);
+
+        return root;
+
+
     }
 };
 // @lc code=end
