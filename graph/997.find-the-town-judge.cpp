9396db9febb5924de7596833120c6bc9fb7c87fd
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-04-29 21:18:21 +0530

    Started with Leetcode :997 Find the Town Judge

---
+++ graph/997.find-the-town-judge.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=997 lang=cpp
+ *
+ * [997] Find the Town Judge
+ */
+
+// @lc code=start
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        
+    }
+};
+// @lc code=end
+
