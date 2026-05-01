f8a18c301360f7231ce0d39ca108b3a09c52c905
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-05-01 22:24:03 +0530

    Started with Leetcode: 567 Permutation in String

---
+++ Strings/567.permutation-in-string.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=567 lang=cpp
+ *
+ * [567] Permutation in String
+ */
+
+// @lc code=start
+#include <string>
+using namespace std;
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        
+    }
+};
+// @lc code=end
+
