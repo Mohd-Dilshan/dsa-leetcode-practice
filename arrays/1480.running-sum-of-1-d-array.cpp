c0d1313c19b6c625f9d03878e4145408bc73c46e
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-05-02 15:38:52 +0530

    Started with Leetcode :33 Search in Rotated Sorted Array

---
+++ arrays/1480.running-sum-of-1-d-array.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=1480 lang=cpp
+ *
+ * [1480] Running Sum of 1d Array
+ */
+
+#include <vector>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        for (int i = 1; i < nums.size(); ++i){
+            nums[i] += nums[i-1];
+        }
+        return nums;
+    }
+};
+// @lc code=end
+
+
---
+++ binary_S/33.search-in-rotated-sorted-array.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=33 lang=cpp
+ *
+ * [33] Search in Rotated Sorted Array
+ */
+
+// @lc code=start
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+    }
+};
+// @lc code=end
+
