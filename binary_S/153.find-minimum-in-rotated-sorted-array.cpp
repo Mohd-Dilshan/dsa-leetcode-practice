699da5ff7264219dc5ea15a502358fb629549875
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-05-03 22:47:26 +0530

    Solved Leetcode :153 Find Minimum in Rotated Sorted Array

--- binary_S/153.find-minimum-in-rotated-sorted-array.cpp
+++ binary_S/153.find-minimum-in-rotated-sorted-array.cpp
@@ -10,7 +10,20 @@ using namespace std;
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        
+       int n = nums.size() ;
+       
+       int l = 0, r = n - 1 ;
+
+       while(l<r) {
+        int mid = l +(r-l)/2;
+
+        if (nums[mid] > nums[r]) {
+            l = mid +1; // move right
+        } else {
+            r = mid; // move left
+        }
+       }
+       return nums[r];
     }
 };
 // @lc code=end
