a39e35283435b9c161e916efa7b2427f2ce70687
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-05-04 21:42:55 +0530

    Started with Leetcode :34 Find First and Last Position of Element

---
+++ binary_S/34.find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=34 lang=cpp
+ *
+ * [34] Find First and Last Position of Element in Sorted Array
+ */
+
+// @lc code=start
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        
+        
+    }
+};
+// @lc code=end
+
