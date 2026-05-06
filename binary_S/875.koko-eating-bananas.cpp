5f75501a5e069dabb65d15e65b38da0c8c21a842
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-05-06 14:12:49 +0530

    Started with Leetcode :875 Koko Eating Bananas

---
+++ binary_S/875.koko-eating-bananas.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=875 lang=cpp
+ *
+ * [875] Koko Eating Bananas
+ */
+
+// @lc code=start
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        
+    }
+};
+// @lc code=end
+
