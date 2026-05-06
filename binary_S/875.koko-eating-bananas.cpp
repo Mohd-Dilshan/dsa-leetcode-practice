fc63514aeaea7d9543e08554e182547f33200c30
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-05-06 15:02:02 +0530

    Solved Leetcode :875 Koko Eating Bananas

--- binary_S/875.koko-eating-bananas.cpp
+++ binary_S/875.koko-eating-bananas.cpp
@@ -10,8 +10,35 @@
 using namespace std;
 class Solution {
 public:
+
+    bool canEatAll(vector <int> & piles, int mid, int h) {
+        int actualHour = 0;
+
+        for (int &x :piles) {
+            actualHour += x/mid; // hours
+            if (x%mid != 0) {
+                actualHour++;
+            }
+        }
+        return actualHour <= h ;
+    }
+
     int minEatingSpeed(vector<int>& piles, int h) {
-        
+        int n = piles.size();
+
+        int  l =1;
+        int r = *max_element(begin(piles), end(piles));
+
+        while (l < r) {
+            int mid  = l + (r-l) /2;  // per hour can eat mid bananas
+
+            if (canEatAll(piles, mid, h)) {
+                r = mid; 
+            } else {
+                l = mid +1;
+            }
+        }
+        return l ;
     }
 };
 // @lc code=end
