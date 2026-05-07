577e191723dec33afd0839e6bbcdecf6d6febcba
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-05-07 23:14:36 +0530

    Solved Leetcode :155 Min Stack

--- stack_MonoStack/155.min-stack.cpp
+++ stack_MonoStack/155.min-stack.cpp
@@ -9,26 +9,83 @@
 using namespace std;
 class MinStack {
 public:
+
+    // approach 1: sc:o(2*n)
+    // stack<pair<int, int >> s;
+
+    // MinStack() {
+        
+    // }
     
+    // void push(int val) {
+    //     if (s.empty() ){
+    //         s.push({val,val});
+    //     } else {
+    //         int minVal = min (val, s.top().second);
+    //         s.push({val, minVal});
+    //     }
+    // }
+    
+    // void pop() {
+    //     s.pop();
+        
+    // }
+    
+    // int top() {
+    //     return s.top().first;
+        
+    // }
+    
+    // int getMin() {
+    //     return s.top().second;
+        
+    // }
+
+
+
+    //approach 2 , tc :(O(n)); sc O(n)
+    stack<long long > s;
+    long long int minVal;
+
     MinStack() {
         
     }
     
     void push(int val) {
-        
+        if (s.empty()){
+            s.push(val);
+            minVal = val;
+        } else {
+            if(val<minVal) {
+                s.push((long long)2*val-minVal);
+                minVal= val;
+            } else {
+                s.push(val);
+            }
+        }
     }
     
     void pop() {
+        if (s.top() < minVal) {
+            minVal = 2 * minVal - s.top();
+        }
+        s.pop();
         
     }
     
     int top() {
-        
+        if (s.top() < minVal){
+            return minVal;
+        }
+        return s.top();
     }
     
     int getMin() {
+        return minVal;
         
     }
+
+
 };
 
 /**
