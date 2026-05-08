b6069bf8df48011d18c373654246782d067fdc81
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-05-08 21:38:26 +0530

    Solved Leetcode: 739 daily temperatures

--- stack_MonoStack/739.daily-temperatures.cpp
+++ stack_MonoStack/739.daily-temperatures.cpp
@@ -6,11 +6,29 @@
 
 // @lc code=start
 #include<vector>
+#include<stack>
 using namespace std;
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-          int n = temperatures.size();
+        int n = temperatures.size();
+          
+        stack<int> st;
+
+        vector<int> result(n);
+
+        for(int i = n-1; i>=0; i--) {
+            while(!st.empty() && temperatures[i] >= temperatures[st.top()]) {
+                st.pop();
+            }
+            if (st.empty()){
+                result[i] = 0;
+            } else {
+                result[i] = st.top() - i; 
+            }
+            st.push(i);
+        }
+        return result;
     }
 };
 // @lc code=end
