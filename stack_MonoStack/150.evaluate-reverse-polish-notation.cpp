7e1186599420a5be2844e8583acdf6e281fce594
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-05-09 21:16:27 +0530

    Started with Leetcode :150 Evaluate Reverse Polish Notation

---
+++ stack_MonoStack/150.evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=150 lang=cpp
+ *
+ * [150] Evaluate Reverse Polish Notation
+ */
+
+// @lc code=start
+#include<vector>
+#include<string>
+using namespace std;
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        
+    }
+};
+// @lc code=end
+
