992333de47739db95becbbea77c10a4d6c8c2d35
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-05-10 19:15:25 +0530

    Started with Leetcode:496 Next Greater Element I

---
+++ stack_MonoStack/496.next-greater-element-i.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=496 lang=cpp
+ *
+ * [496] Next Greater Element I
+ */
+
+// @lc code=start
+#include <vector>
+#include <stack>
+using namespace std;
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        
+    }
+};
+// @lc code=end
+
