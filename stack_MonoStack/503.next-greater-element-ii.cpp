c682687d74fcf4cdc4feb12c2c311900f15cb677
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-05-11 20:40:19 +0530

    Started with Leetcode: 503 next greater element II

---
+++ stack_MonoStack/503.next-greater-element-ii.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=503 lang=cpp
+ *
+ * [503] Next Greater Element II
+ */
+
+// @lc code=start
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        
+    }
+};
+// @lc code=end
+
