7c6070c79c9454fcb76aedf5841704f560c057a4
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-05-12 18:37:24 +0530

    Soved Leetcode : 556 next gereater element

--- Strings/556.next-greater-element-iii.cpp
+++ Strings/556.next-greater-element-iii.cpp
@@ -5,10 +5,41 @@
  */
 
 // @lc code=start
+#include<string>>
+#include<algorithm>
+#include<climits>
+using namespace std;
 class Solution {
 public:
     int nextGreaterElement(int n) {
-        
+        string s = to_string(n);
+        // find the pivot
+        int i = s.size() -2;
+
+        while (i>= 0 && s[i] >= s[i+1]) {
+            --i;
+        }
+        // no greater el exists
+        if (i<0) {
+            return -1;
+        }
+        //find next greater el
+        int j = s.size() -1;
+        while (s[j] <= s[i] ) {
+            --j;
+        }
+        //swap pivot and next greater digit
+         swap(s[i], s[j]);
+
+        //  Reverse suffix
+        reverse(s.begin() + i + 1, s.end());
+
+        // Convert to long long to avoid overflow
+        long long ans = stoll(s);
+
+        return (ans > INT_MAX) ? -1 : (int)ans;
+
+
     }
 };
 // @lc code=end
