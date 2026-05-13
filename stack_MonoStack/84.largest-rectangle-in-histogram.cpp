8d253cba7503309ca6717522483126baf8487988
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-05-13 21:52:55 +0530

    Started with Leetcode :84 Largest Rectangle in Histogram

---
+++ stack_MonoStack/84.largest-rectangle-in-histogram.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=84 lang=cpp
+ *
+ * [84] Largest Rectangle in Histogram
+ */
+
+// @lc code=start
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        
+    }
+};
+// @lc code=end
+
