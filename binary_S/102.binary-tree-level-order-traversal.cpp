c9e9a53516670362f4d96474507c2210fb0dcf5d
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-05-14 20:14:00 +0530

    Solved Leetcode:102 Binary Tree Level Order Traversal

--- binary_S/102.binary-tree-level-order-traversal.cpp
+++ binary_S/102.binary-tree-level-order-traversal.cpp
@@ -17,12 +17,42 @@
  * };
  */
 
- #include<vector>
- using namespace std;
+#include<vector>
+#include<queue>
+using namespace std;
+
 class Solution {
 public:
+
     vector<vector<int>> levelOrder(TreeNode* root) {
-        
+        vector<vector<int>> v;
+
+        if (root == nullptr){
+            return v;
+        }
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+
+        while(!q.empty()){
+            int x = q.size();
+            vector<int>v1;
+
+            while(x--){
+                TreeNode* front = q.front();
+                q.pop();
+                v1.push_back(front->val);
+                if (front->left != nullptr){
+                    q.push(front->left);
+                }
+                if (front->right != nullptr){
+                    q.push(front->right);
+                }
+            }
+            v.push_back(v1);
+        }
+        return v;
     }
 };
 // @lc code=end
