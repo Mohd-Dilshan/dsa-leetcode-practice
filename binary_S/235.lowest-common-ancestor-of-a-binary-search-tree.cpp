277c2059b56e9dd036986703a90ac69c0f24d2df
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-05-16 12:07:26 +0530

    Solved Leetcode:235 Lowest Common Ancestor of a BST

--- binary_S/235.lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ binary_S/235.lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -14,11 +14,21 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-
+#include<vector>
+using namespace std;
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        
+        if (root == nullptr) return nullptr;
+
+        if (root->val > p->val && root->val > q->val ) {
+            return lowestCommonAncestor(root->left, p, q);
+        } else if (root->val < p->val && root->val < q->val) {
+            return lowestCommonAncestor(root->right, p, q);
+        } else {
+            return root;
+        }
+
     }
 };
 // @lc code=end
