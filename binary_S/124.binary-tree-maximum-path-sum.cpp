26e9936d0467dc193f282f1310263b575c611a38
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-05-17 17:06:39 +0530

    Solved Leetcode: 124 Binary Tree Maximum Path Sum

--- binary_S/124.binary-tree-maximum-path-sum.cpp
+++ binary_S/124.binary-tree-maximum-path-sum.cpp
@@ -16,10 +16,35 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+#include<climits>
+using namespace std;
 class Solution {
 public:
+    int maxSum;
+
+    int solve(TreeNode* root) {
+        if (root == nullptr ) {
+            return 0;
+        }
+        int l = solve(root->left);
+        int r = solve(root->right);
+
+        int bestsomebeforRoot = l+ r+ root->val;
+
+        int bestSum = max(l, r) + root->val ;
+        int onlyrootBest = root->val;
+
+        maxSum = max({maxSum, bestsomebeforRoot, bestSum, onlyrootBest});
+
+        return max(bestSum, onlyrootBest);
+    }
+
     int maxPathSum(TreeNode* root) {
+        maxSum = INT_MIN;
         
+        solve(root);
+        return maxSum;
+
     }
 };
 // @lc code=end
