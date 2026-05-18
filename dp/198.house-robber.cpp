68832aea1c1b3941ca616f8ddc4b6a8df577026a
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-05-18 23:05:06 +0530

    Solved Leetcode:198 House Robber

--- dp/198.house-robber.cpp
+++ dp/198.house-robber.cpp
@@ -6,12 +6,53 @@
 
 // @lc code=start
 #include<vector>
+#include <cstring>
 using namespace std;
 class Solution {
 public:
-    int rob(vector<int>& nums) {
+
+    // recursion and memoization- Top Down
+    // int t[101];
+    // int solve(vector<int>& nums, int i, int n) {
+    //     if (i >= n) return 0;
+
+    //     if (t[i] != -1) return t[i];
+
+    //     int steal = nums[i] + solve(nums, i+2, n);
+    //     int skip = solve(nums, i+1, n);
+
+    //     return max (steal, skip);
+    // }
+
+    // int rob(vector<int>& nums) {
+    //     int n = nums.size();
+    //     memset(t, -1, sizeof(t));
+    //     return solve(nums, 0, n);
+
+    // }
+
+    // Bottom- up
+    int rob (vector<int>& nums) {
+        int n = nums.size();
+
+        if (n == 1) return nums[0];
+
+        vector<int> t(n+1, 0);  //t[i]= max stolen money till ith house
+
+        t[0] = 0;   // no house: i= 0
         
+        t[1] = nums[0];  // 1house : i = 1
+
+        for(int i = 2; i<=n; i++) {
+            int steal = nums[i-1] + t[i-2];
+            int skip = t[i-1];
+
+            t[i] = max(steal, skip);
+        }
+        return t[n];
     }
+
 };
+
 // @lc code=end
 
