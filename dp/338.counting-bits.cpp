f16e07fe5b1c72e991ce6b83e8256defcd80330d
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-05-21 22:36:43 +0530

    Solve Leetcode:338.counting-bits

--- dp/338.counting-bits.cpp
+++ dp/338.counting-bits.cpp
@@ -5,10 +5,25 @@
  */
 
 // @lc code=start
+#include<vector>
+using namespace std;
 class Solution {
 public:
     vector<int> countBits(int n) {
-        
+        vector<int> result(n+1);
+
+        if(n == 0)
+            return result;
+        result[0] = 0; // Binary of 0 has 0 number of bits set as 1
+
+        for (int i=1; i<=n; i++){
+            if (i%2 != 0) {
+                result[i] = result[i/2] + 1;
+            } else {
+                result[i] = result[i/2];
+            }
+        }
+        return result;
     }
 };
 // @lc code=end
