b8fb308db1bc54d37a87b8ae86e9293ab94949bd
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-05-23 23:25:56 +0530

    Started with Leetcode:416 partition equal subset sum

---
+++ dp/416.partition-equal-subset-sum.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=416 lang=cpp
+ *
+ * [416] Partition Equal Subset Sum
+ */
+
+// @lc code=start
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        
+    }
+};
+// @lc code=end
+
