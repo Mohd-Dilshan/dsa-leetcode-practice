c3c213a6b91c05f4f1b5e1dbd45c41a45231c78c
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-05-25 23:40:48 +0530

    Solved Leetcode :1791 find the center of start graph

--- graph/1791.find-center-of-star-graph.cpp
+++ graph/1791.find-center-of-star-graph.cpp
@@ -6,11 +6,43 @@
 
 // @lc code=start
 #include <vector>
+#include<unordered_map>
 using namespace std;
 class Solution {
 public:
-    int findCenter(vector<vector<int>>& edges) {
+    // int findCenter(vector<vector<int>>& edges) {
         
+    //     unordered_map<int, int> degree;
+
+    //      for(auto &vec : edges) {
+    //         int u = vec[0];
+    //         int v = vec[1];
+
+    //         degree[u]++;
+    //         degree[v]++;
+    //     }
+    //     for (auto &it :degree) {
+    //         if(it.second == edges.size()) {
+    //             return it.first;
+    //         }
+    //     }
+    //     return -1;
+    // }
+
+
+
+    //method 2
+    int findCenter(vector<vector<int>>& edges) {
+        vector<int> first  = edges[0];
+        vector<int> second = edges[1];
+
+
+        if(first[0] == second[0] || first[0] == second[1]) {
+            return first[0];
+        }
+        return first[1];
+
+
     }
 };
 // @lc code=end
