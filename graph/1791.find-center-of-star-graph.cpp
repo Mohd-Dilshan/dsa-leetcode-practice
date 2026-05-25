1184799372098d47a14cffc609121622345227d2
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-05-25 23:33:02 +0530

    Started with Leetcode :1791 find the center of start graph

---
+++ graph/1791.find-center-of-star-graph.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=1791 lang=cpp
+ *
+ * [1791] Find Center of Star Graph
+ */
+
+// @lc code=start
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        
+    }
+};
+// @lc code=end
+
