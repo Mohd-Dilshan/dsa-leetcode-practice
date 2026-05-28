417415321f68f12f685ae11028b5ca1ca9c99c9f
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-05-28 22:46:07 +0530

    Solves Leetcode :206 reverse linked list

--- linkedList/206.reverse-linked-list.cpp
+++ linkedList/206.reverse-linked-list.cpp
@@ -18,7 +18,9 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
+
         if (!head || !head ->next) return head;
+        
         ListNode* newHead = reverseList(head->next);
         head->next->next = head;
         head->next = nullptr;
