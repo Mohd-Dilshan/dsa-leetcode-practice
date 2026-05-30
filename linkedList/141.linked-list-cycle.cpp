478e17a1b9e889286560785a8974c5863dce57f8
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-05-30 22:11:38 +0530

    Solved Leetcode:141 linked list cycle

--- linkedList/141.linked-list-cycle.cpp
+++ linkedList/141.linked-list-cycle.cpp
@@ -16,7 +16,17 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        
+        if(head == nullptr) return false;
+
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            if (slow == fast) return true;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return false;
     }
 };
 // @lc code=end
