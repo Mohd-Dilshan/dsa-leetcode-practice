d014634d68ff4b28f45f0298cba56342e3af1da9
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-05-31 21:46:12 +0530

    Soved Leetcode :876 middle of the linked list

--- linkedList/876.middle-of-the-linked-list.cpp
+++ linkedList/876.middle-of-the-linked-list.cpp
@@ -18,7 +18,14 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
         
+        while(fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
     }
 };
 // @lc code=end
