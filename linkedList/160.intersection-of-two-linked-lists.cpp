cf71c2b63facff8b80ee9a00284f00e85ca7f06c
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-06-02 21:56:45 +0530

    Solved Leetcode:160 intersection of two linked list

--- linkedList/160.intersection-of-two-linked-lists.cpp
+++ linkedList/160.intersection-of-two-linked-lists.cpp
@@ -13,10 +13,31 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-
+#include<set>
+using namespace std;
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        // pproach Used: Hashing
+        // Time Complexity: O(m + n)
+        // Space Complexity: O(m) (extra space used for storing nodes in a set)
+        set<ListNode*>st;
+        ListNode* temp = headA;
+
+        while(temp!=nullptr) {
+            st.insert(temp);
+            temp = temp->next;
+        }
+        temp = headB;
+        while(temp != nullptr) {
+            if(st.find(temp) != st.end()) {
+                return temp;
+            }
+            temp = temp -> next;
+        }
+        return nullptr ;
+
+
     }
 };
 // @lc code=end
