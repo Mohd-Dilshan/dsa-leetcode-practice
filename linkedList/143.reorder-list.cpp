dac9d8d3a09aa2c60be4b3a91a35086a361f8e3e
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-06-05 22:14:19 +0530

    Solved Leetcode:143 Reorder List

--- linkedList/143.reorder-list.cpp
+++ linkedList/143.reorder-list.cpp
@@ -17,9 +17,66 @@
  */
 class Solution {
 public:
+    // approach -1
+    // ListNode* reverseLL(ListNode* head) {
+
+    //     if(head == nullptr || head->next == nullptr) {
+    //         return head;
+    //     }
+    //     ListNode* last = reverseLL(head->next);
+    //     head->next->next = head;
+    //     head -> next = nullptr;
+    //     return last;
+    // }
+
+    // void reorderList(ListNode* head) {
+    //     ListNode* slow = head;
+    //     ListNode* fast = head;
+    //     while(fast && fast->next) {
+    //         slow = slow->next;
+    //         fast = fast->next->next;
+    //     }
+
+    //     ListNode* rev = reverseLL(slow);
+    //     ListNode* curr = head;
+
+    //     while(rev -> next != nullptr) {
+    //         ListNode* tempcurr = curr ->next;
+    //         curr -> next = rev;
+ 
+    //         ListNode* temprev = rev->next;
+    //         rev -> next = tempcurr;
+
+    //         curr = tempcurr;
+    //         rev = temprev;
+    //     }
+    // }
+
+    // approach 2
     void reorderList(ListNode* head) {
+        stack<ListNode*> st;
         
+        ListNode* curr = head;
+        while(curr) {
+            st.push(curr);
+            curr = curr->next;
+        }
+        
+        int k = st.size()/2;
+        curr = head;
+        while(k--) {
+            ListNode* topNode = st.top();
+            st.pop();
+            
+            ListNode* temp = curr->next;
+            curr->next = topNode;
+            topNode->next = temp;
+            curr = temp;
+        }
+        
+        curr->next = NULL;
     }
+
 };
 // @lc code=end
 
