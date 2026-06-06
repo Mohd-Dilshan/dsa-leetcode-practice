e497c4f08b15f0670ee1cb124064dbaef482bfda
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-06-06 22:41:08 +0530

    Solved Leetcode:142 Linked List Cycle II

--- linkedList/142.linked-list-cycle-ii.cpp
+++ linkedList/142.linked-list-cycle-ii.cpp
@@ -16,7 +16,28 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
+        if(head == nullptr || (head->next == nullptr)) 
+            return nullptr;
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow == fast) 
+                break;
+        }
+        if(slow != fast)
+            return nullptr;
         
+        ListNode* P = head;
+
+        while(P!= slow) {
+            P= P->next;
+            slow = slow->next;
+        }
+        return P;
     }
 };
 // @lc code=end
