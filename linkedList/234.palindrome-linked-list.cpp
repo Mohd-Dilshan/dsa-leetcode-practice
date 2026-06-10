be12bb155091d94fcc2c94f95894a55b346a5a86
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-06-10 21:56:09 +0530

    Solved Leetcode:234 Palindrome linked list

--- linkedList/234.palindrome-linked-list.cpp
+++ linkedList/234.palindrome-linked-list.cpp
@@ -18,6 +18,31 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
+
+        ListNode* p = head;
+
+        stack<int> s;
+
+        while(p!=NULL){
+
+            s.push(p->val);
+
+            p = p->next;
+
+        }
+
+        while(head && !s.empty()){
+
+            if(head->val != s.top()) return false;
+
+            head = head->next;
+
+            s.pop();
+
+        }
+
+        return true;
+
     }
 };
 // @lc code=end
