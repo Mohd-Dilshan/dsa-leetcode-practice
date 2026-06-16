b70d3585529595bd16bc796d157b09d2fe86769f
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-06-16 10:57:56 +0530

    Solved Leetcode:133 Clone graph

--- graph/133.clone-graph.cpp
+++ graph/133.clone-graph.cpp
@@ -25,11 +25,122 @@ public:
     }
 };
 */
-
+#include<unordered_map>
+using namespace std;
 class Solution {
 public:
-    Node* cloneGraph(Node* node) {
+    //Approach-1 DFS (Using unordered_map)
+    unordered_map<Node*, Node*> mp;
+    
+    // DFS helper: traverse original node and build neighbor links in clone
+    void DFS(Node* node, Node* clone_node) {
         
+        for(Node* n : node->neighbors) {
+            
+            if(mp.find(n) == mp.end()) {
+                
+                Node* clone = new Node(n->val);
+                mp[n] = clone;
+                clone_node->neighbors.push_back(clone);
+                
+                DFS(n, clone);
+                
+            } else {
+                
+                // neighbor already cloned: reuse existing clone
+                clone_node->neighbors.push_back(mp[n]);   
+            }   
+        }  
+    }
+    
+    // main entry: clone the graph starting from `node`
+    Node* cloneGraph(Node* node) {
+        if(!node)
+            return NULL;
+        
+        mp.clear();
+        
+        //cloned the given node
+        Node* clone_node = new Node(node->val);
+        
+        mp[node] = clone_node;
+        
+        DFS(node, clone_node);
+        
+        return clone_node;
+    }
+
+
+    //Approach-2 BFS (Using unordered_map)
+    unordered_map<Node*, Node*> mp;
+    
+    void DFS(Node* node, Node* clone_node) {
+        
+        for(Node* n : node->neighbors) {
+            
+            if(mp.find(n) == mp.end()) {
+                
+                Node* clone = new Node(n->val);
+                mp[n] = clone;
+                clone_node->neighbors.push_back(clone);
+                
+                DFS(n, clone);
+                
+            } else {
+                
+                clone_node->neighbors.push_back(mp[n]);
+                
+            }   
+        }
+    }
+    
+    void BFS(queue<Node*> &que) {
+        
+        while(!que.empty()) {
+            
+            Node* node = que.front();
+            Node* clone_node = mp[node];
+            que.pop();
+            
+            for(Node* n : node->neighbors) {
+            
+                if(mp.find(n) == mp.end()) {
+
+                    Node* clone = new Node(n->val);
+                    mp[n] = clone;
+                    clone_node->neighbors.push_back(clone);
+
+                    //DFS(n, clone);
+                    que.push(n);
+
+                } else {
+
+                    clone_node->neighbors.push_back(mp[n]);
+
+                }
+            
+            }
+            
+        }
+        
+    }
+    
+    Node* cloneGraph(Node* node) {
+        if(!node)
+            return NULL;
+        
+        mp.clear();
+        
+        //cloned the given node
+        Node* clone_node = new Node(node->val);
+        
+        mp[node] = clone_node;
+        
+        queue<Node*> que;
+        que.push(node);
+        BFS(que);
+        
+        return clone_node;
     }
 };
 // @lc code=end
