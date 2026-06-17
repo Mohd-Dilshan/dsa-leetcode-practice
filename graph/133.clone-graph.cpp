30b16cfd1f2f96dd255b316cb4c28786d48e7a7f
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-06-17 07:29:57 +0530

    Started with Leetcode :547. Number of Provinces

--- graph/133.clone-graph.cpp
+++ graph/133.clone-graph.cpp
@@ -29,46 +29,46 @@ public:
 using namespace std;
 class Solution {
 public:
-    //Approach-1 DFS (Using unordered_map)
-    unordered_map<Node*, Node*> mp;
+    // //Approach-1 DFS (Using unordered_map)
+    // unordered_map<Node*, Node*> mp;
     
-    // DFS helper: traverse original node and build neighbor links in clone
-    void DFS(Node* node, Node* clone_node) {
+    // // DFS helper: traverse original node and build neighbor links in clone
+    // void DFS(Node* node, Node* clone_node) {
         
-        for(Node* n : node->neighbors) {
+    //     for(Node* n : node->neighbors) {
             
-            if(mp.find(n) == mp.end()) {
+    //         if(mp.find(n) == mp.end()) {
                 
-                Node* clone = new Node(n->val);
-                mp[n] = clone;
-                clone_node->neighbors.push_back(clone);
+    //             Node* clone = new Node(n->val);
+    //             mp[n] = clone;
+    //             clone_node->neighbors.push_back(clone);
                 
-                DFS(n, clone);
+    //             DFS(n, clone);
                 
-            } else {
+    //         } else {
                 
-                // neighbor already cloned: reuse existing clone
-                clone_node->neighbors.push_back(mp[n]);   
-            }   
-        }  
-    }
+    //             // neighbor already cloned: reuse existing clone
+    //             clone_node->neighbors.push_back(mp[n]);   
+    //         }   
+    //     }  
+    // }
     
-    // main entry: clone the graph starting from `node`
-    Node* cloneGraph(Node* node) {
-        if(!node)
-            return NULL;
+    // // main entry: clone the graph starting from `node`
+    // Node* cloneGraph(Node* node) {
+    //     if(!node)
+    //         return NULL;
         
-        mp.clear();
+    //     mp.clear();
         
-        //cloned the given node
-        Node* clone_node = new Node(node->val);
+    //     //cloned the given node
+    //     Node* clone_node = new Node(node->val);
         
-        mp[node] = clone_node;
+    //     mp[node] = clone_node;
         
-        DFS(node, clone_node);
+    //     DFS(node, clone_node);
         
-        return clone_node;
-    }
+    //     return clone_node;
+    // }
 
 
     //Approach-2 BFS (Using unordered_map)
