0c61ddb65d27f7e2986849d96ecb605a2e098c91
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-06-21 14:12:54 +0530

    Started withb Leetcode:1857 Largest color value in a directed graph

---
+++ graph/1857.largest-color-value-in-a-directed-graph.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1857 lang=cpp
+ *
+ * [1857] Largest Color Value in a Directed Graph
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int largestPathValue(string colors, vector<vector<int>>& edges) {
+        
+    }
+};
+// @lc code=end
+
