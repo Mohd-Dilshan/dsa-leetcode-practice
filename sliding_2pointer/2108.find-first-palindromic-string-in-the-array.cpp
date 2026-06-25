f51408a94fe5727387c1bf4436914123f3783921
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-06-25 22:28:42 +0530

    Solved Leetcode :2108. Find First Palindromic String in the Array

--- sliding_2pointer/2108.find-first-palindromic-string-in-the-array.cpp
+++ sliding_2pointer/2108.find-first-palindromic-string-in-the-array.cpp
@@ -8,6 +8,23 @@
 class Solution {
 public:
     string firstPalindrome(vector<string>& words) {
+        //approach:-1
+        // for (string &word : words) {
+        //     if (word == string(rbegin(word), rend(word))) {
+        //         return word;
+        //     }
+        // }
+        // return "";
+
+
+        //app:-2
+        for (string &word : words) {
+            int n = word.size();
+            if (equal(word.begin(),word.begin() + n/2, word.rbegin())) {
+                return word;
+            }
+        }
+        return "";
         
     }
 };
