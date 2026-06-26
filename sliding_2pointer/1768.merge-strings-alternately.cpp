f39b8812bed5496a54d40b37a3911fd86baf7170
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-06-26 21:16:28 +0530

    Solved Leetcode: 1768 Merge strings alternately

--- sliding_2pointer/1768.merge-strings-alternately.cpp
+++ sliding_2pointer/1768.merge-strings-alternately.cpp
@@ -5,10 +5,26 @@
  */
 
 // @lc code=start
+#include <algorithm>
+#include <string>
+using namespace std;
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        
+        int n=word1.size();
+        int m=word2.size();
+        int i=0;
+        string word="";
+        while(i<max(n,m)){
+            if(i<n){
+                word+=word1[i];
+            }
+            if(i<m){
+                word+=word2[i];
+            }
+            i++;
+        }
+        return word;
     }
 };
 // @lc code=end
