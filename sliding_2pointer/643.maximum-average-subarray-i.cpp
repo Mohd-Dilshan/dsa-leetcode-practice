7efa80853815885c9ebabf344dd88db49a771c2d
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-06-29 21:53:36 +0530

    Solved Leetcode : 643. Maximum Average Subarray I

--- sliding_2pointer/643.maximum-average-subarray-i.cpp
+++ sliding_2pointer/643.maximum-average-subarray-i.cpp
@@ -8,7 +8,17 @@
 class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
-        
+        //sliding window
+        int sum = 0;
+        double res;
+        for(int i=0; i<k; i++) sum += nums[i];
+        res = sum;
+        for(int i=k; i<nums.size(); i++){
+            sum = sum - nums[i-k]; //1
+            sum = sum + nums[i]; //2
+            if(sum>res) res = sum;
+        }
+        return res/k;
     }
 };
 // @lc code=end
