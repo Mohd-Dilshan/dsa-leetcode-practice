ede96a2d3964c496b6fa686d18cad1a5b9dfc6ac
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-06-30 08:26:14 +0530

    Started with Leetcode : 1456. Maximum Number of Vowels in a Substring of Given Length

---
+++ sliding_2pointer/1456.maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1456 lang=cpp
+ *
+ * [1456] Maximum Number of Vowels in a Substring of Given Length
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxVowels(string s, int k) {
+        
+    }
+};
+// @lc code=end
+
