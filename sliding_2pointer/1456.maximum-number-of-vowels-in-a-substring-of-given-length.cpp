644a60ba67513d0699bf989b8836c261e7785128
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-06-30 08:36:18 +0530

    Solved Leetcode : 1456. Maximum Number of Vowels in a Substring of Given Length

--- sliding_2pointer/1456.maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ sliding_2pointer/1456.maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -7,8 +7,33 @@
 // @lc code=start
 class Solution {
 public:
+    bool isVowel(char &ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
+    
     int maxVowels(string s, int k) {
+        int n = s.length();
         
+        int maxVowelsCount  = 0;
+        int count = 0;
+        int i = 0, j = 0;
+        
+        while(j < n) {
+            
+            if(isVowel(s[j]))
+                count++;
+            
+            if(j-i+1 == k) {
+                maxVowelsCount = max(maxVowelsCount, count);
+                if(isVowel(s[i]))
+                    count--;
+                i++;
+            }
+            
+            j++;
+        }
+        
+        return maxVowelsCount;
     }
 };
 // @lc code=end
