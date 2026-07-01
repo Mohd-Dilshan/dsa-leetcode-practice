38c3ad726d805bc73d1be14a64580ac4be99897e
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-07-01 11:18:16 +0530

    Started with Leetcode :1876. Substrings of Size Three with Distinct Characters

---
+++ sliding_2pointer/1876.substrings-of-size-three-with-distinct-characters.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1876 lang=cpp
+ *
+ * [1876] Substrings of Size Three with Distinct Characters
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int countGoodSubstrings(string s) {
+        
+    }
+};
+// @lc code=end
+
