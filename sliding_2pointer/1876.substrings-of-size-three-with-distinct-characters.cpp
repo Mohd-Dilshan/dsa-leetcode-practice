a5ebb61ba26ceec4442f2a507d67230b9a4f1217
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-07-01 11:25:42 +0530

    Solved Leetcode:1876. Substrings of Size Three with Distinct Characters

--- sliding_2pointer/1876.substrings-of-size-three-with-distinct-characters.cpp
+++ sliding_2pointer/1876.substrings-of-size-three-with-distinct-characters.cpp
@@ -8,7 +8,29 @@
 class Solution {
 public:
     int countGoodSubstrings(string s) {
-        
+        unordered_map<char, int> mp;
+        int ans = 0;
+        int k = 3;
+
+        for (int i = 0; i < s.size(); i++) {
+            mp[s[i]]++;
+
+            if (i < k - 1)
+                continue;
+
+            if (mp.size() == k) {
+                ans++;
+            }
+
+            int ind = i - k + 1;
+
+            mp[s[ind]]--;
+            if (mp[s[ind]] == 0) {
+                mp.erase(s[ind]);
+            }
+        }
+
+        return ans;
     }
 };
 // @lc code=end
