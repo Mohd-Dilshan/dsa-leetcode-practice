81eeb9d5ecdf309c1f80e84c5e3263a5585c8878
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-07-02 11:13:52 +0530

    Solved Leetcode:169. Majority Element

--- arrays/169.majority-element.cpp
+++ arrays/169.majority-element.cpp
@@ -9,39 +9,39 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         // brute force o(n*n)
-        // int n = nums.size();
-        // for (int val : nums) {
-        //     int freq = 0;
-        //     for (int el : nums){
-        //         if(el == val) {
-        //             freq++;
-        //         }
-        //     }
-        //     if(freq > n/2) {
-        //         return val;
-        //     }
-        // }
-        // return -1;
+        int n = nums.size();
+        for (int val : nums) {
+            int freq = 0;
+            for (int el : nums){
+                if(el == val) {
+                    freq++;
+                }
+            }
+            if(freq > n/2) {
+                return val;
+            }
+        }
+        return -1;
 
 
         // optimal: using sorting o(nlogn)
-        // int n = nums.size();
+        int n = nums.size();
 
-        // sort(nums.begin(), nums.end());
-        // int freq = 1, ans = nums[0];
+        sort(nums.begin(), nums.end());
+        int freq = 1, ans = nums[0];
 
-        // for (int i = 1; i<n; i++) {
-        //     if(nums[i] == nums[i-1]) {
-        //         freq++;
-        //     } else {
-        //         freq = 1;
-        //         ans = nums[i];
-        //     }
-        //     if(freq > n/2){
-        //         return ans;
-        //     }
-         // }
-        // return ans;
+        for (int i = 1; i<n; i++) {
+            if(nums[i] == nums[i-1]) {
+                freq++;
+            } else {
+                freq = 1;
+                ans = nums[i];
+            }
+            if(freq > n/2){
+                return ans;
+            }
+         }
+        return ans;
 
 
         //Moore's Voting Algo ; TC: o(n)
