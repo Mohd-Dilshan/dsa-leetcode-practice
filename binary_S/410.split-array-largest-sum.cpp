7d3414fe8ec3cca9e05ffa43956ba8ba05a59218
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-07-10 11:01:43 +0530

    Started with Leetcode :410. Split Array Largest Sum

---
+++ binary_S/410.split-array-largest-sum.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=410 lang=cpp
+ *
+ * [410] Split Array Largest Sum
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int splitArray(vector<int>& nums, int k) {
+        
+    }
+};
+// @lc code=end
+
