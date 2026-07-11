a56b98092de7597be5b6a6924002b9361668ebdd
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-07-11 13:09:28 +0530

    solved Leetcode :1137. N-th Tribonacci Number

--- dp/1137.n-th-tribonacci-number.cpp
+++ dp/1137.n-th-tribonacci-number.cpp
@@ -5,11 +5,71 @@
  */
 
 // @lc code=start
+#include <cstring>
+using namespace std;
 class Solution {
 public:
-    int tribonacci(int n) {
+    //rec+mem
+    // int t[38];
+    // int find(int n) {
+    //     if(n == 0)
+    //         return 0;
+    //     if(n == 1 || n == 2)
+    //         return 1;
+            
+    //     if(t[n] != -1) {
+    //         return t[n];
+    //     }
         
+    //     int a = find(n-1);
+    //     int b = find(n-2);
+    //     int c = find(n-3);
+        
+    //     return t[n] = a+b+c;
+    // }
+    
+    // int tribonacci(int n) {
+    //     memset(t, -1, sizeof(t));
+    //     return find(n);
+    // }
+
+
+    //bottom-up; tc:O(n)/ O(1); sc: O(1)
+    // int tribonacci (int n) {
+    //     int t[38];
+    //     t[0] = 0;
+    //     t[1] = 1;
+    //     t[2] = 1;
+
+    //     for (int i = 3; i<= 37; i++) {
+    //         t[i] = t[i-1]+ t[i-2] + t[i-3];
+    //     }
+    //     return t[n];
+    // }
+
+
+    //constant space
+    int tribonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+        
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int d = 0;
+        
+        for (int i = 3; i <= n; i++) {
+            d = a + b + c; // Calculate current sequence value
+            a = b;         // Shift window forward
+            b = c;
+            c = d;
+        }
+        
+        return d;
     }
+
+
 };
+
 // @lc code=end
 
