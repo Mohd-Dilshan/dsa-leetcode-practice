c8bf0b66d5cac60cd00ccd1550d35cc18b41dbb2
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-07-17 22:27:23 +0530

    Solved Leetcode :424.Longest Repeating Character Replacement

--- sliding_2pointer/424.longest-repeating-character-replacement.cpp
+++ sliding_2pointer/424.longest-repeating-character-replacement.cpp
@@ -9,7 +9,24 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         
+        unordered_map<char, int> count;
+        
+        int start = 0, end = 0, res = 0, maxFreq = 0;
+        
+        while(end < s.size()){
+            count[s[end]]++;
+            maxFreq = max(maxFreq, count[s[end]]);
+            if(end - start + 1 - maxFreq > k){
+                count[s[start]]--;
+                start++;
+            }
+            
+            res = max(res, end - start + 1);
+            end++;
+        }
+        return res;
     }
 };
+
 // @lc code=end
 
