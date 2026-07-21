9f8cd44b385083c54b0af91d4eca21655fa4273c
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-07-21 10:17:17 +0530

    Started with Leetcode:336. Palindrome Pairs

---
+++ Strings/336.palindrome-pairs.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=336 lang=cpp
+ *
+ * [336] Palindrome Pairs
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> palindromePairs(vector<string>& words) {
+        
+    }
+};
+// @lc code=end
+
