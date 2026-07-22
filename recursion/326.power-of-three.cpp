91004dd85381e4693491590817854385d644d898
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-07-22 10:39:12 +0530

    Solved Leetcode:326 power of three

--- recursion/326.power-of-three.cpp
+++ recursion/326.power-of-three.cpp
@@ -7,9 +7,30 @@
 // @lc code=start
 class Solution {
 public:
+    // approach-1
+    // Time Complexity: O(log3 n)
+    // Space Complexity: O(1)
+
+
     bool isPowerOfThree(int n) {
-        
+        if(n <= 0) {
+            return false;
+        }
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+        return n == 1;
     }
+
+
+    // approach-2
+    // Time Complexity: O(1)
+    // Space Complexity: O(1)
+
+    bool isPowerOfThree(int n) {
+        return n > 0 && 1162261467 % n == 0;
+    }
+
 };
 // @lc code=end
 
