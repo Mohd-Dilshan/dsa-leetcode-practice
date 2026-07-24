5f6d19768bc51109d02079758246878dd8c820e4
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-07-24 09:05:26 +0530

    Started with Leetcode:1545. Find Kth Bit in Nth Binary String

---
+++ recursion/1545.find-kth-bit-in-nth-binary-string.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1545 lang=cpp
+ *
+ * [1545] Find Kth Bit in Nth Binary String
+ */
+
+// @lc code=start
+class Solution {
+public:
+    char findKthBit(int n, int k) {
+        
+    }
+};
+// @lc code=end
+
