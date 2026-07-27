64132ef443a1fc2403713ab40735ad8c3f3d0de3
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-07-27 21:41:40 +0530

    Solved Leetcode:297. Serialize and Deserialize Binary Tree

--- Tree/297.serialize-and-deserialize-binary-tree.cpp
+++ Tree/297.serialize-and-deserialize-binary-tree.cpp
@@ -16,15 +16,30 @@
  */
 class Codec {
 public:
-
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-        
+        return !root ? " null" : " " + to_string(root->val) + serialize(root->left) + serialize(root->right);
     }
 
-    // Decodes your encoded data to tree.
+	// Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
+        istringstream ss(data);
+        return buildTree(ss);
+    }
+    
+private:
+    TreeNode* buildTree(istringstream& ss) {
+        string s;
+        ss >> s;
         
+        if (s == "null")
+			return NULL;
+        
+        TreeNode* node = new TreeNode(stoi(s));
+        node->left = buildTree(ss);
+        node->right = buildTree(ss);
+            
+        return node;
     }
 };
 
