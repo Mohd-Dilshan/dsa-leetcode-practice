2c28c50d436fc3fba61ca475d3acf8004ffe0eb3
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-07-28 18:00:28 +0530

    solved Leetcode : 682 baseball game

--- stack_MonoStack/682.baseball-game.cpp
+++ stack_MonoStack/682.baseball-game.cpp
@@ -5,11 +5,39 @@
  */
 
 // @lc code=start
+
+// T.C= O(n); S.C= O(n)
 class Solution {
 public:
-    int calPoints(vector<string>& operations) {
-        
+    int calPoints(vector<string>& ops) {
+        stack<int> s;
+        for(auto x : ops){
+            if(x == "C"){
+                s.pop();
+            }
+            else if(x == "D"){
+                int a = s.top();
+                s.push(a*2);
+            }
+            else if(x == "+"){
+                int a = s.top();
+                s.pop();
+                int y = s.top();
+                s.push(a);
+                s.push(a+y);
+            }
+            else{
+                s.push(stoi(x));
+            }
+        }
+        int ans = 0;
+        while(!s.empty()){
+            ans+=s.top();
+            s.pop();
+        }
+        return ans;
     }
 };
+
 // @lc code=end
 
