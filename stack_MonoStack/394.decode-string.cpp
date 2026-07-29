152cfc6796e65b81fbbfa9c14e34e3fd34a56434
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-07-29 19:22:15 +0530

    Solved Leetcode :394. Decode String

--- stack_MonoStack/394.decode-string.cpp
+++ stack_MonoStack/394.decode-string.cpp
@@ -8,8 +8,34 @@
 class Solution {
 public:
     string decodeString(string s) {
-        
+        stack<int> count;
+        stack<string> strs;
+        string curr = "";
+        int num = 0;
+
+        for (auto c : s) {
+            if (isdigit(c)) {
+                num = num * 10 + (c - '0');
+            } else if (c == '[') {
+                count.push(num);
+                strs.push(curr);
+                num = 0;
+                curr = "";
+            } else if (c == ']') {
+                int rep = count.top();
+                count.pop();
+                string temp = curr;
+                curr = strs.top();
+                strs.pop();
+                while (rep--) {
+                    curr += temp;
+                }
+            } else {
+                curr += c;
+            }
+        }
+        return curr;
     }
-};
+};   
 // @lc code=end
 
