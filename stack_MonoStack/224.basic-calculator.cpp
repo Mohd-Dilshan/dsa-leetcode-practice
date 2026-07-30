81d66a91377977787369368144bcd880eea18464
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-07-30 17:08:24 +0530

    Solved Leetcode :224. Basic Calculator

--- stack_MonoStack/224.basic-calculator.cpp
+++ stack_MonoStack/224.basic-calculator.cpp
@@ -5,10 +5,43 @@
  */
 
 // @lc code=start
+
 class Solution {
 public:
     int calculate(string s) {
+        stack<int> st;
+        long long number = 0;
+        long long result = 0;
+        int sign   = 1;
         
+        for(int i = 0; i<s.length(); i++) {
+            if(isdigit(s[i])) {
+                number  = 10*number + (s[i] - '0');
+            } else if(s[i] == '+') {
+                result += sign*number;
+                number  = 0;
+                sign = 1; 
+            } else if(s[i] == '-') {
+                result += sign*number;
+                number  = 0;
+                sign = -1; 
+            } else if(s[i] == '(') {
+                st.push(result);
+                st.push(sign);
+                result = 0;
+                number = 0;
+                sign = 1;
+            } else if(s[i] == ')') {
+                result += sign*number;
+                number = 0;
+                int top = st.top(); st.pop();
+                result *= top;
+                top = st.top(); st.pop();
+                result += top;
+            }
+        }
+        result += (sign*number);
+        return result;
     }
 };
 // @lc code=end
