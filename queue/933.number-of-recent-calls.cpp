e309cb6a7c63ac8f72a1ade347de76a2ddd36fd7
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-07-31 23:07:19 +0530

    Solved Leetcode :933. Number of Recent Calls

--- queue/933.number-of-recent-calls.cpp
+++ queue/933.number-of-recent-calls.cpp
@@ -7,11 +7,18 @@
 // @lc code=start
 class RecentCounter {
 public:
+    queue<int>q;
     RecentCounter() {
         
     }
     
     int ping(int t) {
+        q.push(t);
+        int low = t-3000;
+        
+        while (!q.empty() && q.front() < low)q.pop();
+
+        return q.size();
         
     }
 };
