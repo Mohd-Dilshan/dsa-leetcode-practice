02f5d968877d05ff71b7f7d8750eba17120bb354
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-08-08 22:20:56 +0530

    Implement singleNumber function for Leetcode :137. Single Number II

--- bitManipulation/137.single-number-ii.cpp
+++ bitManipulation/137.single-number-ii.cpp
@@ -8,7 +8,29 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        
+        int result = 0;
+
+        for (int k = 0; k<=31; k++) {
+            int countOnes = 0;
+
+            int temp = (1<<k);
+
+            for (int &num :nums) {
+
+                if((num & temp)== 0) {
+                }
+                else{
+                    countOnes++;
+                }
+            }
+
+            if(countOnes % 3 == 1){
+                result = (result | (temp));
+            }
+
+        }
+        return result;
+
     }
 };
 // @lc code=end
