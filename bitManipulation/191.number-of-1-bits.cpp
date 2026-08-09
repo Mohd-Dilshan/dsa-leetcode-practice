cb04e9b8595c1708aa392d0ef070c228aa1ece8c
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-08-09 23:20:41 +0530

    Refactor hammingWeight implementation using __builtin_popcount for efficiency

--- bitManipulation/191.number-of-1-bits.cpp
+++ bitManipulation/191.number-of-1-bits.cpp
@@ -8,8 +8,55 @@
 class Solution {
 public:
     int hammingWeight(int n) {
-        
+        int count = 0;
+
+        for (int i =31; i>= 0; i--) {
+            if(((n>>i)& 1)== 1) {
+                count++;
+            }
+        }
+        return count;
     }
 };
+
+
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int count = 0;
+
+        while (n){
+            n = (n & (n-1));
+            count++;
+        }
+        return count;
+    }
+};
+
+
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int count = 0;
+
+        while (n){
+            count += (n%2);
+            n /= 2;
+        }
+        return count;
+    }
+};
+
+
+
+
+class Solution {
+public:
+    int hammingWeight(int n) {
+        return __builtin_popcount(n);
+    }
+};
+
+
 // @lc code=end
 
