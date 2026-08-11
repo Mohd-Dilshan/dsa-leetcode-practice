26f4a0a7cce427f6cce1219e509048ae867c5aab
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-08-11 22:00:18 +0530

    Implement countBits function for Leetcode :338. Counting Bits

--- bitManipulation/338.counting-bits.cpp
+++ bitManipulation/338.counting-bits.cpp
@@ -8,7 +8,27 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        
+        vector<int>result(n+1);
+
+        // for (int i = 1; i<=n; i++) {
+        //     result[i] = __builtin_popcount(i);
+        // }
+        // return result;
+
+        if(n==0){
+            return result;
+        }
+        result[0] = 0;
+
+        for (int i = 1; i<=n; i++) {
+            if(i%2 != 0) {
+                result[i] = result[i/2]+1;
+            }
+            else {
+                result[i] = result[i/2];
+            }
+        }
+        return result;
     }
 };
 // @lc code=end
