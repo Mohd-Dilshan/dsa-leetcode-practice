9133b32e060f40c7250eb1a123c9af57c00d9cbb
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-09-09 23:42:17 +0530

    Create 3871.count-commas-in-range-ii.java

---
+++ Potd/3871.count-commas-in-range-ii.java
@@ -0,0 +1,21 @@
+//T.C : O(log1000(n))
+//S.C : O(1)
+
+class Solution {
+    public long countCommas(long n) {
+        long result =0;
+        long lower =1000;
+        long commas = 1;
+        while (lower<=n){
+            long upper = lower*1000-1;
+            if(upper>n) upper = n;
+            
+            long countNum= upper-lower+1;
+            
+            result += (countNum * commas);
+            lower *= 1000;
+            commas++;
+        }
+        return result;
+    }
+}
