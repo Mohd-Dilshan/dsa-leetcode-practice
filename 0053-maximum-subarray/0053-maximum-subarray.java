d920f7cfd2bfd47fbe13d60cf2c9da9823c6c069
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-09-10 22:56:50 +0530

    Update 0053-maximum-subarray.java

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -9,3 +9,20 @@ class Solution {
         return maxSum;
     }
 }
+
+
+//kadane's algo
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum=0;
+        int maxSum=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            maxSum=Math.max(sum,maxSum);
+            if(sum<0){
+                sum=0;
+            }
+        }
+        return maxSum;
+    }
+}
