bffeebb9405d9b84b3699c98d9a409789234b02f
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-09-15 23:44:28 +0530

    Update 217.contains-duplicate.cpp

--- array_str_mixed/217.contains-duplicate.cpp
+++ array_str_mixed/217.contains-duplicate.cpp
@@ -30,3 +30,26 @@ public:
 };
 // @lc code=end
 
+
+
+
+
+
+
+//*******************JAVA***********************
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+      Set<Integer> intSet= new HashSet<>();
+        
+        for(int i =0; i<nums.length; i++){
+            if(intSet.contains(nums[i])) {
+                return true;
+            } else {
+                intSet.add(nums[i]);
+            }
+        }
+        return false;
+        
+    }
+}
+
