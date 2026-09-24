0c37eff7933b8c9bba1bcc3e1c25e9d9f4a15245
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-09-24 22:22:31 +0530

    Update 1876.substrings-of-size-three-with-distinct-characters.cpp

--- sliding_2pointer/1876.substrings-of-size-three-with-distinct-characters.cpp
+++ sliding_2pointer/1876.substrings-of-size-three-with-distinct-characters.cpp
@@ -35,3 +35,25 @@ public:
 };
 // @lc code=end
 
+
+
+************************JAVA*******************
+class Solution {
+    public int countGoodSubstrings(String s) {
+
+        char[] str = s.toCharArray();
+        int count = 0;
+
+        for (int i = 0; i < str.length - 2; i++) {
+
+            if (str[i] != str[i + 1]
+                    && str[i] != str[i + 2]
+                    && str[i + 1] != str[i + 2]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
+
